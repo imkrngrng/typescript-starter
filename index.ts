9e20bace9e9ece18fc4b295898f9734e2d2b974f
kiran gurung <krngrngtamu@gmail.com>
2022-06-05 21:01:11 +0000

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+/**
+ * Install typescript using
+ * npm i -g typescript
+ * check typescript version using
+ * tsc -v
+ * compile typescript file using
+ * tsc ./index.ts
+ */
+
+console.log('typescript installation');
